f853e5a5e4a566d1b5068534a9e693194bec20ca
JacobLinCool <jacoblincool@gmail.com>
2021-10-08 16:47:47 +0800

    fix: warning of wrong scanf type of uint16_t

--- hw1/hw0102.c
+++ hw1/hw0102.c
@@ -2,13 +2,13 @@
 #include <stdint.h>
 
 int main() {
-    int16_t inputs[2] = { 0 };
+    uint16_t inputs[2] = { 0 };
 
     // Input
     printf("Please enter the first  number: ");
-    scanf("%d", &inputs[0]);
+    scanf("%hu", &inputs[0]);
     printf("Please enter the second number: ");
-    scanf("%d", &inputs[1]);
+    scanf("%hu", &inputs[1]);
 
     // Input Validation
     if (inputs[0] < 100 || inputs[0] > 999 || inputs[1] < 100 || inputs[1] > 999) {
@@ -17,14 +17,14 @@ int main() {
     }
 
     // Calculation
-    int16_t sum = inputs[0] + inputs[1];
+    uint16_t sum = inputs[0] + inputs[1];
 
     // Outputs
-    printf("   %d %d %d\n", inputs[0] / 100, inputs[0] % 100 / 10, inputs[0] % 10);
-    printf("+) %d %d %d\n", inputs[1] / 100, inputs[1] % 100 / 10, inputs[1] % 10);
+    printf("   %u %u %u\n", inputs[0] / 100, inputs[0] % 100 / 10, inputs[0] % 10);
+    printf("+) %u %u %u\n", inputs[1] / 100, inputs[1] % 100 / 10, inputs[1] % 10);
     printf("--------\n");
-    if (sum > 999) printf(" %d %d %d %d\n", sum / 1000, sum % 1000 / 100, sum % 100 / 10, sum % 10);
-    else printf("   %d %d %d\n", sum / 100, sum % 100 / 10, sum % 10);
+    if (sum > 999) printf(" %u %u %u %u\n", sum / 1000, sum % 1000 / 100, sum % 100 / 10, sum % 10);
+    else printf("   %u %u %u\n", sum / 100, sum % 100 / 10, sum % 10);
 
     return 0;
 }
--- hw1/hw0103.c
+++ hw1/hw0103.c
@@ -2,16 +2,16 @@
 #include <stdint.h>
 
 int main() {
-    int16_t input;
+    uint16_t input;
 
     printf("Please enter an unsigned 16-bits number: ");
-    scanf("%d", &input);
+    scanf("%hu", &input);
 
-    printf("Before Flip:\n%d_10 = %x_16\n", input, input);
+    printf("Before Flip:\n%u_10 = %x_16\n", input, input);
 
-    int16_t flipped = (input & 0xF000) >> 12 | (input & 0x0F00) >> 4 | (input & 0x00F0) << 4 | (input & 0x000F) << 12;
+    uint16_t flipped = (input & 0xF000) >> 12 | (input & 0x0F00) >> 4 | (input & 0x00F0) << 4 | (input & 0x000F) << 12;
 
-    printf("After Flip:\n%d_10 = %x_16\n", flipped, flipped);
+    printf("After Flip:\n%u_10 = %x_16\n", flipped, flipped);
 
     return 0;
 }
