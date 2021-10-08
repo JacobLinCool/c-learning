1e48d9d44f6d1385eb446e695bdba553ad798cb1
JacobLinCool <jacoblincool@gmail.com>
2021-10-08 17:35:03 +0800

    chore: guard update

--- .guard/index.js
+++ .guard/index.js
@@ -5,16 +5,31 @@ const exec = util.promisify(require("child_process").exec);
 
 (async () => {
     const homeworks = getFolders();
+    const failed = [],
+        warning = [];
     for (const homework of homeworks) {
         try {
-            console.log(`=====\n`);
+            console.log("=====");
             console.log(`Running "make" for ${homework}`);
             const { stdout, stderr } = await exec(`cd ${path.join(__dirname, "../", homework)} && make`);
-            console.log("STDOUT:\n", stdout);
-            console.log("STDERR:\n", stderr);
+            console.log(`STDOUT: \n${stdout}`);
+            console.log(`STDERR: \n${stderr || "No Error. Excellent!"}`);
+            if (stdout.match(/warning/i)) warning.push(homework);
+            if (stdout.match(/error/i)) failed.push(homework);
         } catch (error) {
             console.error(error);
+            failed.push(homework);
         }
+    }
+    console.log("=====");
+    if (failed.length || warning.length) {
+        console.log(`${failed.length} Failed! \n${failed.join("\n")}`);
+        console.log("-----");
+        console.log(`${warning.length} Warnings! \n${warning.join("\n")}`);
+        process.exit(1);
+    } else {
+        console.log("All Checks Passed! Well Done!");
+        process.exit(0);
     }
 })();
 
