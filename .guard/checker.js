f5139d5725207033c09029d11823ebca65a7e429
JacobLinCool <jacoblincool@gmail.com>
2021-10-11 02:03:53 +0800

    feat: add md to pdf conversion

--- .guard/checker.js
+++ .guard/checker.js
@@ -4,16 +4,20 @@ const exec = util.promisify(require("child_process").exec);
 
 async function check(homework) {
     try {
-        console.log("=====");
-        console.log(`Running "make" for ${homework}`);
+        log("=====");
+        log(`Running "make" for ${homework}`);
         const { stdout, stderr } = await exec(`cd ${path.join(__dirname, "../", homework)} && make`);
-        console.log(`STDOUT: \n${stdout}`);
-        console.log(`STDERR: \n${stderr || "No Error. Excellent!"}`);
+        log(`STDOUT: \n${stdout}`);
+        log(`STDERR: \n${stderr || "No Error. Excellent!"}`);
         if (stdout.match(/warning/i)) console.log("WRANING!!");
         if (stdout.match(/error/i)) console.log("ERROR!!!");
     } catch (error) {
         process.exit(1);
     }
+}
+
+function log(...msg) {
+    console.log("[Checker]", ...msg);
 }
 
 module.exports = check;
--- .guard/maker.js
+++ .guard/maker.js
@@ -2,25 +2,37 @@ const fs = require("fs");
 const path = require("path");
 const util = require("util");
 const exec = util.promisify(require("child_process").exec);
+const { mdToPdf } = require("md-to-pdf");
 
 async function make(homework) {
+    log("=====");
+    log(`Generating resources of ${homework}`);
     const cFiles = fs.readdirSync(path.join(__dirname, "../", homework)).filter((f) => f.endsWith(".c"));
     const pdfFiles = fs.readdirSync(path.join(__dirname, "../", homework)).filter((f) => f.endsWith(".pdf"));
 
+    // Makefile
     const makefile = createMakefile(cFiles);
     fs.writeFileSync(path.join(__dirname, "../", homework, "Makefile"), makefile);
 
-    // if README not exist, create it
-    if (!fs.existsSync(path.join(__dirname, "../", homework, "README"))) {
+    // README
+    if (!fs.existsSync(path.join(__dirname, "../", homework, "README")))
         fs.writeFileSync(path.join(__dirname, "../", homework, "README"), createREADME(cFiles));
-    }
 
+    // MD -> PDF
+    if (fs.existsSync(path.join(__dirname, "../", homework, "README.md")))
+        await MDtoPDF(path.join(__dirname, "../", homework, "README.md"), path.join(__dirname, "../", homework, "README.pdf"));
+    const mdFiles = fs.readdirSync(path.join(__dirname, "../", homework)).filter((f) => f.endsWith(".md"));
+    for (const mdFile of mdFiles)
+        await MDtoPDF(path.join(__dirname, "../", homework, mdFile), path.join(__dirname, "../", homework, mdFile.replace(".md", ".pdf")));
+
+    // ZIP
     const { stdout, stderr } = await exec(`cd ${homework} && zip ${homework}.zip README Makefile ${cFiles.join(" ")} ${pdfFiles.join(" ")}`);
-    console.log(stdout);
-    console.error(stderr);
+    log(`STDOUT: \n${stdout}`);
+    log(`STDERR: \n${stderr || "No Error. Excellent!"}`);
     if (fs.existsSync(path.join(__dirname, "../", homework, `${homework}.zip`))) {
         if (!fs.existsSync(path.join(__dirname, "../", "dist"))) fs.mkdirSync(path.join(__dirname, "../", "dist"));
         fs.renameSync(path.join(__dirname, "../", homework, `${homework}.zip`), path.join(__dirname, "../", "dist", `${homework}.zip`));
+        log(`${homework}.zip is generated.`);
     }
 }
 
@@ -56,6 +68,20 @@ function createREADME(cFiles) {
     }
 
     return readme;
+}
+
+async function MDtoPDF(filepath, distpath) {
+    try {
+        const pdf = await mdToPdf({ path: filepath }, { highlight_style: "nord" });
+        if (pdf) fs.writeFileSync(distpath, pdf.content);
+        else throw new Error("PDF Conversion Failed.");
+    } catch (err) {
+        log(err);
+    }
+}
+
+function log(...msg) {
+    console.log("[Maker]", ...msg);
 }
 
 module.exports = make;
