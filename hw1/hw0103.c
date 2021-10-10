fe00c1e3ada487237a069badc5a03cdd44837ade
JacobLinCool <jacoblincool@gmail.com>
2021-10-11 01:00:46 +0800

    fix: add padding to hex

--- hw1/hw0103.c
+++ hw1/hw0103.c
@@ -7,11 +7,11 @@ int main() {
     printf("Please enter an unsigned 16-bits number: ");
     scanf("%hu", &input);
 
-    printf("Before Flip:\n%u_10 = %x_16\n", input, input);
+    printf("Before Flip:\n%u_10 = %04x_16\n", input, input);
 
     uint16_t flipped = (input & 0xF000) >> 12 | (input & 0x0F00) >> 4 | (input & 0x00F0) << 4 | (input & 0x000F) << 12;
 
-    printf("After Flip:\n%u_10 = %x_16\n", flipped, flipped);
+    printf("After Flip:\n%u_10 = %04x_16\n", flipped, flipped);
 
     return 0;
 }
