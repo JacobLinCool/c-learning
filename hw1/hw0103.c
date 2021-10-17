05d8bd06be172ad39d9c4936b9706a6b7814bf8d
JacobLinCool <jacoblincool@gmail.com>
2021-10-17 22:08:00 +0800

    feat(hw1): update comments

--- hw1/hw0103.c
+++ hw1/hw0103.c
@@ -9,6 +9,7 @@ int main() {
 
     printf("Before Flip:\n%u_10 = %04x_16\n", input, input);
 
+    //                 A___ -> ___A             _B__ -> __B_            __C_ -> _C__            ___D -> D___
     uint16_t flipped = (input & 0xF000) >> 12 | (input & 0x0F00) >> 4 | (input & 0x00F0) << 4 | (input & 0x000F) << 12;
 
     printf("After Flip:\n%u_10 = %04x_16\n", flipped, flipped);
