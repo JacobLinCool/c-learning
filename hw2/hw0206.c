0f8881813862ec0c30f3235449dc2f1596d00cc1
JacobLinCool <jacoblincool@gmail.com>
2021-10-21 22:02:46 +0800

    feat(hw2): 0206 add colorful print

--- hw2/hw0206.c
+++ hw2/hw0206.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 
+int front_color = 91;
+
+void ranbow_print(char str[]) {
+    for (int i = 0; str[i] != '\0'; i++) {
+        printf("\033[1;%dm%c\033[0m", front_color, str[i]);
+        front_color = front_color == 96 ? 91 : front_color + 1;
+    }
+}
+
 int main() {
-    // Set style to [Bold] [Red]
-    printf("\033[1;31m");
-
-    // Print "Hello World"
-    printf("Hello World\n");
-
-    // Reset
-    printf("\033[0m");
-
+    ranbow_print("Hello, World!\n");
     return 0;
 }
