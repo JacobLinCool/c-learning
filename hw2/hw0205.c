f38065bb8d2c83e7310b1e5787fd8cffc40a63c5
JacobLinCool <jacoblincool@gmail.com>
2021-10-22 00:02:36 +0800

    feat(hw2): add 0205

---
+++ hw2/hw0205.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+
+int main() {
+    int32_t cards[13] = { 0 };
+
+    for (int8_t i = 0; i < 13; i++) {
+        printf("%d%s card: ", i + 1, (i == 0) ? "st" : (i == 1) ? "nd" : (i == 2) ? "rd" : "th");
+        if (scanf("%d", &cards[i]) != 1 || cards[i] < 1 || cards[i] > 52) {
+            printf("Invalid Input!\n");
+            return 1;
+        }
+    }
+    printf("---\n");
+
+    // suit[0] = "Spades", suit[1] = "Hearts", suit[2] = "Diamonds", suit[3] = "Clubs";
+    int32_t hcp = 0, suit[4] = { 0 };
+
+    // calculate HCP
+    for (int8_t i = 0; i < 13; i++) {
+        if (cards[i] % 13 == 1) hcp += 4;
+        else if (cards[i] % 13 == 0) hcp += 3;
+        else if (cards[i] % 13 == 12) hcp += 2;
+        else if (cards[i] % 13 == 11) hcp += 1;
+    }
+
+    // calculate suit
+    for (int8_t i = 0; i < 13; i++) suit[cards[i] / 13]++;
+
+    printf("HCP: %d pts\n", hcp);
+    printf("Suit: %d-%d-%d-%d\n", suit[0], suit[1], suit[2], suit[3]);
+
+    // Apply Rule 8 is no other conditions are satisfied 
+    char choice[4] = "Pass";
+
+    // Rule 1
+    if (hcp >= 22) strcpy(choice, "2C");
+    // Rule 2
+    else if (suit[0] >= 5 && 13 <= hcp && hcp <= 21) strcpy(choice, "1S");
+    else if (suit[1] >= 5 && 13 <= hcp && hcp <= 21) strcpy(choice, "1H");
+    // Rule 3
+    else if (16 <= hcp && hcp <= 18) strcpy(choice, "1NT");
+    // Rule 4
+    else if (20 <= hcp && hcp <= 21) strcpy(choice, "2NT");
+    // Rule 5
+    else if (suit[2] >= 3 && 13 <= hcp && hcp <= 21) strcpy(choice, "1D");
+    else if (suit[3] >= 3 && 13 <= hcp && hcp <= 21) strcpy(choice, "1C");
+    // Rule 6
+    else if (suit[0] >= 7 && 10 <= hcp && hcp <= 12) strcpy(choice, "3S");
+    else if (suit[1] >= 7 && 10 <= hcp && hcp <= 12) strcpy(choice, "3H");
+    else if (suit[2] >= 7 && 10 <= hcp && hcp <= 12) strcpy(choice, "3D");
+    else if (suit[3] >= 7 && 10 <= hcp && hcp <= 12) strcpy(choice, "3C");
+    // Rule 7
+    else if (suit[0] >= 6 && 10 <= hcp && hcp <= 12) strcpy(choice, "2S");
+    else if (suit[1] >= 6 && 10 <= hcp && hcp <= 12) strcpy(choice, "2H");
+    else if (suit[2] >= 6 && 10 <= hcp && hcp <= 12) strcpy(choice, "2D");
+    else if (suit[3] >= 6 && 10 <= hcp && hcp <= 12) strcpy(choice, "2C");
+
+    printf("The bidding choice : %s\n", choice);
+
+    return 0;
+}
