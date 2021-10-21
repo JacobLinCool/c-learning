ba18b9f5cbbf0f792925a40bcc83dad36009a844
JacobLinCool <jacoblincool@gmail.com>
2021-10-22 01:10:41 +0800

    chore(guard): try to run exe

--- .guard/checker.js
+++ .guard/checker.js
@@ -1,3 +1,4 @@
+const { existsSync, readFileSync } = require("fs");
 const path = require("path");
 const util = require("util");
 const exec = util.promisify(require("child_process").exec);
@@ -15,17 +16,43 @@ async function check(homework) {
             if (stderr) log(`STDERR: \n${stderr}`);
             if (stdout.match(/warning/i)) console.log("WRANING!!");
             if (stdout.match(/error/i)) console.log("ERROR!!!");
-            findFilesByRegex(path.join(__dirname, "../", homework), /hw\d{4}\.c$/)
-                .filter(
-                    (file) =>
-                        findFilesByRegex(path.join(__dirname, "../", homework), new RegExp("^" + path.basename(file).replace(".c", "") + "$")).length !== 1
-                )
-                .forEach((file) => {
-                    log(`Cannot find compiled file [${path.basename(file).replace(".c", "")}] for [${path.basename(file)}]`);
-                });
+
+            // const exes = findFilesByRegex(path.join(__dirname, "../", homework), /hw\d{4}$/);
+            // for (const exe of exes) {
+            //     log(`Running ${exe}`);
+            //     if (!existsSync(exe + ".test")) {
+            //         log(`${exe} does not have test file.`);
+            //         continue;
+            //     }
+            //     const test = readFileSync(exe + ".test", "utf8");
+            //     const [input, output] = test.split("=====").map((part) => part.trim());
+            //     await run(exe, input, output);
+            // }
         } catch (error) {
             process.exit(1);
         }
+    }
+}
+
+async function run(exe, input = "", output = "") {
+    try {
+        const { stdout, stderr } = await exec(`${exe} ${input}`);
+        if (
+            stdout
+                .split("\n")
+                .map((line) => line.trim())
+                .join("\n") !==
+            output
+                .split("\n")
+                .map((line) => line.trim())
+                .join("\n")
+        ) {
+            throw new Error("Output is not correct");
+        }
+        if (stderr) throw new Error(stderr);
+        log(`PASSED: [${exe}]`);
+    } catch (error) {
+        log(`ERROR: [${exe}] `, error);
     }
 }
 
