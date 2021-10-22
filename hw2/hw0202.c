75abe2eea4a7ba43f3fa2f196069d84c0d2e77fe
JacobLinCool <jacoblincool@gmail.com>
2021-10-22 13:25:26 +0800

    fix(hw2): 0202 problematic input condition & types

--- hw2/hw0202.c
+++ hw2/hw0202.c
@@ -2,27 +2,27 @@
 #include <stdint.h>
 
 int main() {
-    int64_t x1, y1, x2, y2;
+    int32_t x1, y1, x2, y2;
 
     printf("First point (x,y): ");
     // Throw an ERROR if the input is not accepted
-    if (scanf("%ld, %ld", &x1, &y1) != 2) {
+    if (scanf("%d, %d", &x1, &y1) != 2) {
         printf("Invalid Input!\n");
         return 1;
     }
 
     printf("Second point (x,y): ");
     // Throw an ERROR if the input is not accepted
-    if (scanf("%ld, %ld", &x2, &y2) != 2) {
+    if (scanf("%d, %d", &x2, &y2) != 2) {
         printf("Invalid Input!\n");
         return 1;
     }
 
-    int64_t perimeter = (((x1 - x2) > 0 ? (x1 - x2) : (x2 - x1)) + ((y1 - y2) > 0 ? (y1 - y2) : (y2 - y1))) * 2;
-    int64_t area = (((x1 - x2) > 0 ? (x1 - x2) : (x2 - x1)) * ((y1 - y2) > 0 ? (y1 - y2) : (y2 - y1)));
+    int64_t perimeter = (((x1 - x2) >= 0 ? (x1 - x2) : (x2 - x1)) + ((y1 - y2) >= 0 ? (y1 - y2) : (y2 - y1))) * 2;
+    int64_t area = (((x1 - x2) >= 0 ? (x1 - x2) : (x2 - x1)) * ((y1 - y2) >= 0 ? (y1 - y2) : (y2 - y1)));
 
     // Throw an ERROR if there is a vertical or horizontal line
-    if (area <= 0 && (x1 != x2 || y1 != y2)) {
+    if (area == 0 && ((x1 != x2) ^ (y1 != y2))) {
         printf("Problematic Input: Two Points Cannot Be In The Same Vertical or Horizontal Line.\n");
         return 1;
     }
