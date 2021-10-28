99b16fcbab0c899c3c7b2d137cdf9bf78bacd2a3
JacobLinCool <jacoblincool@gmail.com>
2021-10-29 01:35:21 +0800

    fix(hw2): 0203

--- hw2/hw0203.c
+++ hw2/hw0203.c
@@ -1,23 +1,22 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <inttypes.h>
 
-int32_t validate_input(int32_t yy, int32_t mm, int32_t dd, int32_t h, int32_t m);
-int32_t calc_total_days(int32_t y1, int32_t m1, int32_t d1, int32_t y2, int32_t m2, int32_t d2);
-int8_t calc_weekday(int32_t y, int32_t m, int32_t d);
-int32_t calc_single(int32_t h1, int32_t m1, int32_t h2, int32_t m2);
+int32_t validate_input(int32_t yy, int32_t mm, int32_t dd, int32_t h, int32_t m); // O(1)
+int64_t calc_total_days(int32_t y1, int32_t m1, int32_t d1, int32_t y2, int32_t m2, int32_t d2); // O(dy)
+int8_t calc_weekday(int32_t y, int32_t m, int32_t d); // O(1)
+int32_t calc_single(int32_t h1, int32_t m1, int32_t h2, int32_t m2); // O(1)
 
 int32_t main() {
     int32_t b_yy, b_mm, b_dd, b_h, b_m, e_yy, e_mm, e_dd, e_h, e_m;
 
     printf("From: ");
-    // Throw an ERROR if the input is not accepted
     if (scanf("%d/%d/%d %d:%d", &b_yy, &b_mm, &b_dd, &b_h, &b_m) != 5) {
         printf("Invalid Input: Only Integers Allowed.\n");
         return 1;
     }
 
     printf("To: ");
-    // Throw an ERROR if the input is not accepted
     if (scanf("%d/%d/%d %d:%d", &e_yy, &e_mm, &e_dd, &e_h, &e_m) != 5) {
         printf("Invalid Input: Only Integers Allowed.\n");
         return 1;
@@ -26,44 +25,51 @@ int32_t main() {
     // printf("[DEBUG] %ld %ld %ld %ld %ld\n", b_yy, b_mm, b_dd, b_h, b_m);
     // printf("[DEBUG] %ld %ld %ld %ld %ld\n", e_yy, e_mm, e_dd, e_h, e_m);
 
-    // Throw an ERROR if the number is not valid
+    // Throw an ERROR if the date-time is not valid
     if (validate_input(b_yy, b_mm, b_dd, b_h, b_m) || validate_input(e_yy, e_mm, e_dd, e_h, e_m)) {
         printf("Invalid Input: Containing Invalid Date(s).\n");
         return 1;
     }
 
     int64_t result = 0;
-    int32_t total_days = calc_total_days(1, 1, 1, e_yy, e_mm, e_dd) - calc_total_days(1, 1, 1, b_yy, b_mm, b_dd);
-    // printf("[DEBUG] total_days: %ld\n", total_days);
+    int64_t total_days = calc_total_days(1, 1, 1, e_yy, e_mm, e_dd) - calc_total_days(1, 1, 1, b_yy, b_mm, b_dd) + 1;
+    // printf("[DEBUG] total_days: %" PRId64 "\n", total_days);
 
-    int8_t weekday = calc_weekday(b_yy, b_mm, b_dd);
-    // printf("[DEBUG] weekday: %u\n", weekday);
+    int8_t b_weekday = calc_weekday(b_yy, b_mm, b_dd);
+    // printf("[DEBUG] b_weekday: %u\n", b_weekday);
 
-    if (total_days < 0) {
+    if (total_days < 1) {
         printf("Invalid Input: End Date is earlier than Start Date.\n");
         return 1;
     }
-    else if (total_days == 0) {
+
+    // full-week between start and end
+    if (total_days - 2 >= 7) {
+        result += ((total_days - 2) / 7) * 40 * 60;
+        total_days -= ((total_days - 2) / 7) * 7;
+    }
+
+    if (total_days == 1) {
         // the start time and end time are in the same day
         result = calc_single(b_h, b_m, e_h, e_m);
         if (result < 0) {
             printf("Invalid Input: End Time is earlier than Start Time.\n");
             return 1;
         }
-        if (weekday == 0 || weekday == 6) result = 0;
+        if (b_weekday == 0 || b_weekday == 6) result = 0;
     }
     else {
-        if (weekday != 0 && weekday != 6) result += calc_single(b_h, b_m, 24, 0);
-        for (int32_t i = 1; i < total_days; i++) {
-            weekday = (weekday + 1) % 7;
-            if (weekday != 0 && weekday != 6) result += calc_single(0, 0, 24, 0);
+        if (b_weekday != 0 && b_weekday != 6) result += calc_single(b_h, b_m, 24, 0);
+        for (int8_t i = 2; i < total_days; i++) {
+            b_weekday = (b_weekday + 1) % 7;
+            if (b_weekday != 0 && b_weekday != 6) result += calc_single(0, 0, 24, 0);
         }
-        weekday = (weekday + 1) % 7;
-        if (weekday != 0 && weekday != 6) result += calc_single(0, 0, e_h, e_m);
+        b_weekday = (b_weekday + 1) % 7;
+        if (b_weekday != 0 && b_weekday != 6) result += calc_single(0, 0, e_h, e_m);
     }
 
-    // printf("[DEBUG] result: %ld\n", result);
-    printf("Working Hours : %ld hours %ld mins.\n", result / 60, result % 60);
+    // printf("[DEBUG] result: %" PRId64 "\n", result);
+    printf("Working Hours : %" PRId64 " hours %" PRId64 " mins.\n", result / 60, result % 60);
 
     return 0;
 }
@@ -87,11 +93,6 @@ int32_t validate_input(int32_t yy, int32_t mm, int32_t dd, int32_t h, int32_t m)
 // @return weekday: Monday = 1, Tuesday = 2, Wednesday = 3, Thursday = 4, Friday = 5, Saturday = 6, Sunday = 0
 int8_t calc_weekday(int32_t y, int32_t m, int32_t d) {
     return (d += m < 3 ? y-- : y - 2, 23 * m / 9 + d + 4 + y / 4 - y / 100 + y / 400) % 7;
-    // m = (m == 1 || m == 2) ? m + 10 : m - 2;
-    // int32_t c = y / 100 - 1;
-    // y -= (m == 1 || m == 2) ? 1 : 0;
-    // int8_t weekday = (((y % 100) * 5 / 4) + m + d - 2 * (c % 4) + 7) % 7;
-    // return weekday;
 }
 
 // calculate the number of days between two dates in one specific year. @return days
@@ -108,11 +109,12 @@ int32_t calc_days_in_year(int32_t yr, int32_t m1, int32_t d1, int32_t m2, int32_
 }
 
 // calculate the number of days between two dates. @return days
-int32_t calc_total_days(int32_t y1, int32_t m1, int32_t d1, int32_t y2, int32_t m2, int32_t d2) {
-    int32_t days = 0;
+int64_t calc_total_days(int32_t y1, int32_t m1, int32_t d1, int32_t y2, int32_t m2, int32_t d2) {
+    int64_t days = 0;
 
     // years between y1 and y2
-    for (int32_t i = y1 + 1; i < y2; i++) days += is_leap(i) + 365;
+    for (int32_t i = y1 + 1; i < y2; i++) days += 365 + is_leap(i);
+    // printf("[DEBUG] days: %" PRId64 "\n", days);
 
     if (y1 == y2) {
         days += calc_days_in_year(y1, m1, d1, m2, d2);
@@ -122,19 +124,20 @@ int32_t calc_total_days(int32_t y1, int32_t m1, int32_t d1, int32_t y2, int32_t 
         days += calc_days_in_year(y2, 1, 1, m2, d2);
     }
 
+    // printf("[DEBUG] days: %ld\n", days);
     return days;
 }
 
 // calculate the working minutes of a single day @return minutes
 int32_t calc_single(int32_t h1, int32_t m1, int32_t h2, int32_t m2) {
     int32_t begin = h1 * 60 + m1, end = h2 * 60 + m2;
+    if (begin > end) return end - begin;
 
     if (begin > 18 * 60 + 30 || end < 9 * 60) return 0;
     if (begin < 9 * 60) begin = 9 * 60;
     if (end > 18 * 60 + 30) end = 18 * 60 + 30;
 
     int32_t diff = end - begin;
-    if (end - begin < 0) return -1;
 
     if (begin <= 12 * 60 && end >= 13 * 60 + 30) diff -= 90;
     else if (begin <= 12 * 60 && end >= 12 * 60) diff -= (end - 12 * 60);
