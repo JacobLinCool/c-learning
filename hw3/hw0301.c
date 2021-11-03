9a84ed05c134ded2c74c657447e192f56080e577
JacobLinCool <jacoblincool@gmail.com>
2021-11-04 00:28:36 +0800

    feat(hw3): add 0301 & 0302

---
+++ hw3/hw0301.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <inttypes.h>
+
+const int DEBUG = 0;
+
+int64_t calc_padding(int64_t n) {
+    int64_t padding = 1;
+    while (n /= 10) padding++;
+    return padding;
+}
+
+void print_tornado(int64_t height, int64_t width) {
+    int64_t max = height * width;
+    int64_t padding = calc_padding(max);
+    int64_t tornado[height][width];
+
+    if (DEBUG) printf("[DEBUG] size of tornado: %" PRId64 "\n", sizeof(tornado));
+
+    // put numbers into tornado in a counter-clockwise direction
+    int64_t x = 0, y = 0, direction = 0, level = 0;
+    for (int64_t i = 0; i < max; i++) {
+        tornado[y][x] = i + 1;
+
+        // move to the next position
+        if (direction == 0) y++;
+        else if (direction == 1) x++;
+        else if (direction == 2) y--;
+        else if (direction == 3) x--;
+
+        // check if we need to change the direction
+        if (direction == 0 && y == height - level - 1) direction = 1;
+        else if (direction == 1 && x == width - level - 1) direction = 2;
+        else if (direction == 2 && y == level) direction = 3;
+        else if (direction == 3 && x == level + 1) direction = 4;
+
+        // check if we need to upgrade the level
+        if (direction == 4) {
+            direction = 0;
+            level++;
+        }
+    }
+
+    // print the tornado
+    for (int64_t i = 0; i < height; i++) {
+        for (int64_t j = 0; j < width; j++) {
+            printf("%*" PRId64 " ", padding, tornado[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+    int64_t width = 0, height = 0;
+
+    printf("Please enter the width  : ");
+    if (scanf("%" SCNd64, &width) != 1 || width <= 0) {
+        printf("Invalid Input. Received: %" PRId64 "\n", width);
+        return 1;
+    }
+
+    printf("Please enter the height : ");
+    if (scanf("%" SCNd64, &height) != 1 || height <= 0) {
+        printf("Invalid Input. Received: %" PRId64 "\n", height);
+        return 1;
+    }
+
+    print_tornado(height, width);
+
+    return 0;
+}
---
+++ hw3/hw0302.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <inttypes.h>
+
+const int DEBUG = 0;
+
+int64_t calc_length(int64_t n) {
+    int64_t length = 1;
+    while (n /= 10) length++;
+    return length;
+}
+
+int main() {
+    int64_t first = 0, second = 0;
+
+    printf("Please enter the first  number: ");
+    if (scanf("%" SCNd64, &first) != 1 || first < 0 || first > INT32_MAX) {
+        printf("Invalid Input. Received: %" PRId64 "\n", first);
+        return 1;
+    }
+
+    printf("Please enter the second number: ");
+    if (scanf("%" SCNd64, &second) != 1 || second < 0 || second > INT32_MAX) {
+        printf("Invalid Input. Received: %" PRId64 "\n", second);
+        return 1;
+    }
+
+    if (DEBUG) printf("[DEBUG] first: %" PRId64 ", second: %" PRId64 "\n", first, second);
+
+    int64_t result = first + second;
+    if (DEBUG) printf("[DEBUG] result: %" PRId64 "\n", result);
+
+    if (result > INT32_MAX) {
+        printf("Overflow Occurred.\n");
+        return 1;
+    }
+
+    int64_t length_first = calc_length(first), length_second = calc_length(second), length_result = calc_length(result);
+    int64_t split_first[length_first], split_second[length_second], split_result[length_result];
+    for (int64_t i = 0; i < length_first; i++) {
+        split_first[i] = first % 10;
+        first /= 10;
+    }
+    for (int64_t i = 0; i < length_second; i++) {
+        split_second[i] = second % 10;
+        second /= 10;
+    }
+    for (int64_t i = 0; i < length_result; i++) {
+        split_result[i] = result % 10;
+        result /= 10;
+    }
+
+
+    for (int64_t i = 0; i < (length_result - length_first) + 1; i++) printf("  ");
+    for (int64_t i = length_first - 1; i >= 0; i--) printf("%2" PRId64, split_first[i]);
+    printf("\n+)");
+    for (int64_t i = 0; i < length_result - length_second; i++) printf("  ");
+    for (int64_t i = length_second - 1; i >= 0; i--) printf("%2" PRId64, split_second[i]);
+    printf("\n");
+    for (int64_t i = 0; i < length_result + 1; i++) printf("--");
+    printf("\n");
+    printf("  ");
+    for (int64_t i = length_result - 1; i >= 0; i--) printf("%2" PRId64, split_result[i]);
+    printf("\n");
+
+    return 0;
+}
