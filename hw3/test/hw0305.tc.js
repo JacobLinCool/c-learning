f80689e75e1aa666297d5d0e637b81f06e099f6e
JacobLinCool <jacoblincool@gmail.com>
2021-11-05 12:04:32 +0800

    test(hw3): fix with -1 and prediction

--- hw3/test/hw0305.tc.js
+++ hw3/test/hw0305.tc.js
@@ -1,9 +1,9 @@
 function acceptable_year() {
-    return 1900 + Math.floor(Math.random() * (2 ** 31 - 1900));
+    return (1900 + Math.floor(Math.random() * (2 ** 16))).toString();
 }
 
 function acceptable_temparature() {
-    return -273.15 + Math.random() * (2 ** 31 + 273.15);
+    return (-273.15 + Math.random() * (2 ** 16 + 273.15)).toFixed(1);
 }
 
 const rules = [
@@ -12,11 +12,12 @@ const rules = [
         text: "2000 29.5 2001 29.7 2002 29.6 2003 31.2 2004 30.5 -1 2022",
     },
     {
-        name: "2 ~ 1000 data points",
+        name: "2 ~ 100 data points",
         generator: () => {
-            const p = 2 + Math.floor(Math.random() * 999);
+            const p = 2 + Math.floor(Math.random() * 99);
             let str = "";
             for (let i = 0; i < p; i++) str += `${acceptable_year()} ${acceptable_temparature()} `;
+            str += "-1 " + acceptable_year();
             return str;
         },
         repeat: 100,
