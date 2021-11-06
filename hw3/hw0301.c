439b50fc8bde666f1c43ee021561433075677e0d
JacobLinCool <jacoblincool@gmail.com>
2021-11-06 14:21:43 +0800

    feat(hw3): 0301 space O(1)

--- hw3/hw0301.c
+++ hw3/hw0301.c
@@ -2,70 +2,72 @@
 #include <stdint.h>
 #include <inttypes.h>
 
+// Print debug messages if DEBUG = 1
 const int DEBUG = 0;
 
-int32_t calc_padding(int64_t n) {
+// The width and height of the matrix
+int64_t width = 0, height = 0;
+
+// Ask for inputs
+int ask(char[], int64_t*);
+
+// Caclulate the digit for a given number
+int32_t calc_digit(int64_t);
+
+// Get the number at a given position
+int64_t get_num(int64_t, int64_t);
+
+// Calculate the level of a given position
+int64_t calc_level(int64_t, int64_t);
+
+int main() {
+    if (!ask("Please enter the width  : ", &width)) return 1;
+    if (!ask("Please enter the height : ", &height)) return 1;
+
+    int32_t padding = calc_digit(width * height);
+
+    for (int64_t i = 0; i < height; i++) {
+        for (int64_t j = 0; j < width; j++) printf("%*" PRId64 " ", padding, get_num(i, j));
+        printf("\n");
+    }
+
+    return 0;
+}
+
+int ask(char q[], int64_t* answer) {
+    printf("%s", q);
+    int eaten = scanf("%" SCNd64, answer);
+    if (eaten == 1 && *answer > 0) return 1;
+    printf("Invalid Input. Received: %" PRId64 "\n", *answer);
+    return 0;
+
+}
+
+int32_t calc_digit(int64_t n) {
     int64_t padding = 1;
     while (n /= 10) padding++;
     return padding;
 }
 
-void print_tornado(int64_t height, int64_t width) {
-    int64_t max = height * width;
-    int32_t padding = calc_padding(max);
-    int64_t tornado[height][width];
+int64_t get_num(int64_t y, int64_t x) {
+    int64_t num = 0;
+    int64_t level = calc_level(y, x);
+    int64_t h = height - 2 * level, w = width - 2 * level;
+    int64_t last_num = height * width - h * w;
 
-    if (DEBUG) printf("[DEBUG] size of tornado: %" PRId64 "\n", sizeof(tornado));
+    if (h == 1 || w == 1) return last_num + 1 + (y - level) + (x - level);
+    else if ((y == level && x != level) || x == width - level - 1) num = last_num + (h + w - 2) + (height - level - y) + (width - level - x - 1);
+    else num = last_num + (y - level + 1) + (x - level);
 
-    // put numbers into tornado in a counter-clockwise direction
-    int64_t x = 0, y = 0, direction = 0, level = 0;
-    for (int64_t i = 0; i < max; i++) {
-        tornado[y][x] = i + 1;
-
-        // check if we need to change the direction
-        if (direction == 0 && y == height - level - 1) direction = 1;
-        else if (direction == 1 && x == width - level - 1) direction = 2;
-        else if (direction == 2 && y == level) direction = 3;
-        else if (direction == 3 && x == level + 1) direction = 4;
-
-        // move to the next position
-        if (direction == 0 || direction == 4) y++;
-        else if (direction == 1) x++;
-        else if (direction == 2) y--;
-        else if (direction == 3) x--;
-
-        // check if we need to upgrade the level
-        if (direction == 4) {
-            direction = 0;
-            level++;
-        }
-    }
-
-    // print the tornado
-    for (int64_t i = 0; i < height; i++) {
-        for (int64_t j = 0; j < width; j++) {
-            printf("%*" PRId64 " ", padding, tornado[i][j]);
-        }
-        printf("\n");
-    }
+    return num;
 }
 
-int main() {
-    int64_t width = 0, height = 0;
+int64_t calc_level(int64_t y, int64_t x) {
+    int64_t level = x < y ? x : y;
+    level = width - x - 1 < level ? width - x - 1 : level;
+    level = height - y - 1 < level ? height - y - 1 : level;
 
-    printf("Please enter the width  : ");
-    if (scanf("%" SCNd64, &width) != 1 || width <= 0) {
-        printf("Invalid Input. Received: %" PRId64 "\n", width);
-        return 1;
-    }
+    if (DEBUG) printf("(%" PRId64 ", %" PRId64 ") -> %" PRId64 "\n", y, x, level);
 
-    printf("Please enter the height : ");
-    if (scanf("%" SCNd64, &height) != 1 || height <= 0) {
-        printf("Invalid Input. Received: %" PRId64 "\n", height);
-        return 1;
-    }
-
-    print_tornado(height, width);
-
-    return 0;
+    return level;
 }
