9e884203729f2b5d8da816316b0cef40ba650561
JacobLinCool <jacoblincool@gmail.com>
2021-11-18 17:45:01 +0800

    feat(hw4): triangle circumscribed

--- hw4/hw0403.c
+++ hw4/hw0403.c
@@ -5,32 +5,57 @@
 int main() {
 
     printf(
-        "Initial States: \nCheck: %d\nPerimeter: %lf\nArea: %lf\nDegree 1: %lf\nDegree 2: %lf\nDegree 3: %lf\nInscribed (X, Y): (%lf, %lf)\nInscribed Area: %lf\n---\n", 
-        check(), 
-        get_perimeter(), 
-        get_area(), 
-        get_1_degree(), 
-        get_2_degree(), 
-        get_3_degree(), 
-        get_inscribed_center_x(), 
-        get_inscribed_center_y(), 
-        get_inscribed_center_area()
+        "Initial States: \nCheck: %d\nPerimeter: %lf\nArea: %lf\nDegree 1: %lf\nDegree 2: %lf\nDegree 3: %lf\nInscribed (X, Y): (%lf, %lf)\nInscribed Area: %lf\nCircumscribed (X, Y): (%lf, %lf)\nCircumscribed Area: %lf\n---\n",
+        check(),
+        get_perimeter(),
+        get_area(),
+        get_1_degree(),
+        get_2_degree(),
+        get_3_degree(),
+        get_inscribed_center_x(),
+        get_inscribed_center_y(),
+        get_inscribed_center_area(),
+        get_circumscribed_center_x(),
+        get_circumscribed_center_y(),
+        get_circumscribed_center_area()
     );
 
     set_1_point(0.0, 0.0);
     set_2_point(0.0, 3.0);
     set_3_point(4.0, 0.0);
     printf(
-        "Points Set: \nCheck: %d\nPerimeter: %lf\nArea: %lf\nDegree 1: %lf\nDegree 2: %lf\nDegree 3: %lf\nInscribed (X, Y): (%lf, %lf)\nInscribed Area: %lf\n---\n", 
-        check(), 
-        get_perimeter(), 
-        get_area(), 
-        get_1_degree(), 
-        get_2_degree(), 
-        get_3_degree(), 
-        get_inscribed_center_x(), 
-        get_inscribed_center_y(), 
-        get_inscribed_center_area()
+        "Points (0, 0), (0, 3), (4, 0): \nCheck: %d\nPerimeter: %lf\nArea: %lf\nDegree 1: %lf\nDegree 2: %lf\nDegree 3: %lf\nInscribed (X, Y): (%lf, %lf)\nInscribed Area: %lf\nCircumscribed (X, Y): (%lf, %lf)\nCircumscribed Area: %lf\n---\n",
+        check(),
+        get_perimeter(),
+        get_area(),
+        get_1_degree(),
+        get_2_degree(),
+        get_3_degree(),
+        get_inscribed_center_x(),
+        get_inscribed_center_y(),
+        get_inscribed_center_area(),
+        get_circumscribed_center_x(),
+        get_circumscribed_center_y(),
+        get_circumscribed_center_area()
+    );
+
+    set_1_point(0.0, 6.0);
+    set_2_point(-sqrt(36.0 - 9.0), -3.0);
+    set_3_point(sqrt(36.0 - 9.0), -3.0);
+    printf(
+        "Points (0, 6), (-(36.0 - 9.0)**0.5, -3), ((36.0 - 9.0)**0.5, -3): \nCheck: %d\nPerimeter: %lf\nArea: %lf\nDegree 1: %lf\nDegree 2: %lf\nDegree 3: %lf\nInscribed (X, Y): (%lf, %lf)\nInscribed Area: %lf\nCircumscribed (X, Y): (%lf, %lf)\nCircumscribed Area: %lf\n---\n",
+        check(),
+        get_perimeter(),
+        get_area(),
+        get_1_degree(),
+        get_2_degree(),
+        get_3_degree(),
+        get_inscribed_center_x(),
+        get_inscribed_center_y(),
+        get_inscribed_center_area(),
+        get_circumscribed_center_x(),
+        get_circumscribed_center_y(),
+        get_circumscribed_center_area()
     );
     return 0;
 }
--- hw4/triangle.c
+++ hw4/triangle.c
@@ -140,15 +140,55 @@ double get_inscribed_center_area(void) {
 }
 
 double get_circumscribed_center_x(void) {
+    if (!check()) return 0.0;
 
+    double a = p2.x - p1.x;
+    double b = p2.y - p1.y;
+    double c = p3.x - p1.x;
+    double d = p3.y - p1.y;
+
+    double e = a * (p1.x + p2.x) + b * (p1.y + p2.y);
+    double f = c * (p1.x + p3.x) + d * (p1.y + p3.y);
+    double g = 2 * (a * (p3.y - p2.y) - b * (p3.x - p2.x));
+
+    if (g == 0) return NAN;
+
+    double center_x = (d * e - b * f) / g;
+
+    return center_x;
 }
 
 double get_circumscribed_center_y(void) {
+    if (!check()) return 0.0;
 
+    double a = p2.x - p1.x;
+    double b = p2.y - p1.y;
+    double c = p3.x - p1.x;
+    double d = p3.y - p1.y;
+
+    double e = a * (p1.x + p2.x) + b * (p1.y + p2.y);
+    double f = c * (p1.x + p3.x) + d * (p1.y + p3.y);
+    double g = 2 * (a * (p3.y - p2.y) - b * (p3.x - p2.x));
+
+    if (g == 0) return NAN;
+
+    double center_y = (a * f - c * e) / g;
+
+    return center_y;
 }
 
 double get_circumscribed_center_area(void) {
+    if (!check()) return -1.0;
 
+    int64_t triangle_area = get_area();
+
+    double a = sqrt(pow(p2.x - p3.x, 2) + pow(p2.y - p3.y, 2));
+    double b = sqrt(pow(p1.x - p3.x, 2) + pow(p1.y - p3.y, 2));
+    double c = sqrt(pow(p1.x - p2.x, 2) + pow(p1.y - p2.y, 2));
+
+    double r = (a * b * c) / (4 * triangle_area);
+
+    return r * r * M_PI;
 }
 
 // Any application that can be written in JavaScript, will eventually be written in JavaScript.
